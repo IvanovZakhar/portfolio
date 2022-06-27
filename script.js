28a892f2f5c9929d3f9bf4e20d5513b0a3943818
3axap <uvanov977777@gmail.com>
2022-06-27 22:32:55 +0300

    get project, feedback, faveico and footer

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', ()=>{ 
+
+    // const img = document.querySelectorAll('.img-card'),
+    //       text = document.querySelectorAll('.card-text');
+    
+    //  text.forEach(e => {
+    //     e.style.position = 'absolute';
+    //     e.style.zIndex = '1';
+    //  });   
+    
+    //  img.forEach(e => {
+    //     e.style.position = 'absolute';
+    //     e.style.zIndex = '2';
+    //     e.addEventListener('mouseover', (e)=>{
+    //         console.log(e.target);
+    //     });
+    //  });
+    
+});
